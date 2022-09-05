6a34421b5ee081d51c1255fd6cdd7e92c447d52d
gucal <gucalaliriza@outlook.com>
2022-09-05 14:54:21 +0300

    topbar and sidebar imported

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { TopbarComponent } from './topbar.component';
+import { SidebarComponent } from './sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent, TopbarComponent, SidebarComponent],
   imports: [CommonModule],
 })
 export class LayoutModule {}
