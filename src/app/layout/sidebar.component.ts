19acc8358a62f27683c4c1380d8673c40eff8bf0
gucal <gucalaliriza@outlook.com>
2022-09-07 15:21:46 +0300

    badge add

--- src/app/layout/sidebar.component.ts
+++ src/app/layout/sidebar.component.ts
@@ -4,6 +4,7 @@ interface Item {
   id: number;
   name: string;
   icon: string;
+  badge: number;
 }
 
 interface MenuItem {
@@ -33,26 +34,31 @@ export class SidebarComponent implements OnInit {
           id: 1,
           name: 'Dashboard',
           icon: 'pi pi-home',
+          badge: 0,
         },
         {
           id: 2,
           name: 'Bookmarks',
           icon: 'pi pi-bookmark',
+          badge: 0,
         },
         {
           id: 3,
           name: 'Team',
           icon: 'pi pi-users',
+          badge: 0,
         },
         {
           id: 4,
           name: 'Messages',
           icon: 'pi pi-comments',
+          badge: 3,
         },
         {
           id: 5,
           name: 'Calendar',
           icon: 'pi pi-calendar',
+          badge: 0,
         },
       ],
     },
@@ -63,16 +69,19 @@ export class SidebarComponent implements OnInit {
           id: 6,
           name: 'Projects',
           icon: 'pi pi-folder',
+          badge: 0,
         },
         {
           id: 7,
           name: 'Reports',
           icon: 'pi pi-chart-bar',
+          badge: 0,
         },
         {
           id: 8,
           name: 'Projects',
           icon: 'pi pi-cog',
+          badge: 0,
         },
       ],
     },
