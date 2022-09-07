f7f83f18632eed5068e8b3dcefd178cc4e1e362e
gucal <gucalaliriza@outlook.com>
2022-09-07 11:11:39 +0300

    mobile menu collapse

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  isMobileMenu?: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  collapseMenu() {
+    this.isMobileMenu = !this.isMobileMenu;
   }
-
 }
--- src/app/layout/topbar.component.ts
+++ src/app/layout/topbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
 })
 export class TopbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @Output() collapseMenuClick = new EventEmitter<any>();
+
+  collapseClick() {
+    this.collapseMenuClick.emit();
   }
-
 }
