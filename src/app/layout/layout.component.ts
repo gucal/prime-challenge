3ae744b113d82f88e3cebfc40d0bfe82df8b5759
gucal <gucalaliriza@outlook.com>
2022-09-07 12:13:27 +0300

    mobil menu close function

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -12,7 +12,7 @@ export class LayoutComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  collapseMenu() {
-    this.isMobileMenu = true;
+  collapseMenu(isVisible: boolean) {
+    this.isMobileMenu = isVisible;
   }
 }
